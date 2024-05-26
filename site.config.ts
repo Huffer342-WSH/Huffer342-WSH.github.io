893e0d8cf8a3cb44f0cdb60298aff2d253d969b6
Huffer342-WSH <718007138@qq.com>
2024-05-26 16:12:48 +0800

    [new] 配置Waline评论

--- site.config.ts
+++ site.config.ts
@@ -4,96 +4,31 @@ export default defineSiteConfig({
   url: 'https://huffer342-wsh.github.io/',
   lang: 'zh-CN',
   title: 'Huffer\'s blog',
+  subtitle: ' ',
+
   author: {
     name: 'Huffer',
-    avatar:'https://avatars.githubusercontent.com/u/78366424?v=4'
+    avatar:'https://avatars.githubusercontent.com/u/78366424?v=4',
+    status: {
+      emoji: '😅',
+    },
   },
+
   description: ' ',
+
+  mediumZoom: { enable: true },
+
   social: [
-    // {
-    //   name: 'RSS',
-    //   link: '/atom.xml',
-    //   icon: 'i-ri-rss-line',
-    //   color: 'orange',
-    // },
-    // {
-    //   name: 'QQ 群 1050458482',
-    //   link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
-    //   icon: 'i-ri-qq-line',
-    //   color: '#12B7F5',
-    // },
     {
       name: 'GitHub',
       link: 'https://github.com/Huffer342-WSH',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
-    // {
-    //   name: '微博',
-    //   link: 'https://weibo.com/jizhideyunyoujun',
-    //   icon: 'i-ri-weibo-line',
-    //   color: '#E6162D',
-    // },
-    // {
-    //   name: '豆瓣',
-    //   link: 'https://www.douban.com/people/yunyoujun/',
-    //   icon: 'i-ri-douban-line',
-    //   color: '#007722',
-    // },
-    // {
-    //   name: '网易云音乐',
-    //   link: 'https://music.163.com/#/user/home?id=247102977',
-    //   icon: 'i-ri-netease-cloud-music-line',
-    //   color: '#C20C0C',
-    // },
-    // {
-    //   name: '知乎',
-    //   link: 'https://www.zhihu.com/people/yunyoujun/',
-    //   icon: 'i-ri-zhihu-line',
-    //   color: '#0084FF',
-    // },
-    // {
-    //   name: '哔哩哔哩',
-    //   link: 'https://space.bilibili.com/1579790',
-    //   icon: 'i-ri-bilibili-line',
-    //   color: '#FF8EB3',
-    // },
-    // {
-    //   name: '微信公众号',
-    //   link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
-    //   icon: 'i-ri-wechat-2-line',
-    //   color: '#1AAD19',
-    // },
-    // {
-    //   name: 'Twitter',
-    //   link: 'https://twitter.com/YunYouJun',
-    //   icon: 'i-ri-twitter-line',
-    //   color: '#1da1f2',
-    // },
-    // {
-    //   name: 'Telegram Channel',
-    //   link: 'https://t.me/elpsycn',
-    //   icon: 'i-ri-telegram-line',
-    //   color: '#0088CC',
-    // },
-    // {
-    //   name: 'E-Mail',
-    //   link: 'mailto:me@yunyoujun.cn',
-    //   icon: 'i-ri-mail-line',
-    //   color: '#8E71C1',
-    // },
-    // {
-    //   name: 'Travelling',
-    //   link: 'https://www.travellings.cn/go.html',
-    //   icon: 'i-ri-train-line',
-    //   color: 'var(--va-c-text)',
-    // },
+    
   ],
 
-  search: {
-    enable: true,
-    type: 'fuse',
-  },
+
 
   sponsor: {
     enable: false,
@@ -119,4 +54,19 @@ export default defineSiteConfig({
       },
     ],
   },
+
+  search: {
+    enable: true,
+    type: 'fuse',
+  },
+
+  comment: {
+    enable: true,
+  },
+
+  statistics: {
+    enable: true,
+  },
+
+
 })
