c89ab7c69926729bbde8c5c2ed81f34322165254
Huffer342-WSH <718007138@qq.com>
2026-04-08 14:56:24 +0800

    site: 添加项目展示

---
+++ pages/projects/index.md
@@ -0,0 +1,14 @@
+---
+title: 我的小项目
+layout: projects
+projects:
+  chrome-extension:
+    title: chrome扩展
+    emoji: 🧩
+    collection:
+      - name: ChatGPT Markdown Copier
+        emoji: 📄
+        desc: 复制ChatGPT回复为Markdown
+        github: Huffer342-WSH/ChatGPT-Markdown-Copier
+        url: https://github.com/Huffer342-WSH/ChatGPT-Markdown-Copier
+---
--- theme.config.ts
+++ theme.config.ts
@@ -2,13 +2,21 @@ import { defineThemeConfig } from 'valaxy-theme-yun/node'
 export default defineThemeConfig({
   type: 'strato',
 
-    banner: {
-        enable: false,
-      title: '',
-        cloud: {
-          enable: false,
-        },
-      },
+  banner: {
+    enable: false,
+    title: '',
+    cloud: {
+      enable: false,
+    },
+  },
 
- 
+  pages: [
+    {
+      name: '项目列表',
+      url: '/projects',
+      icon: 'i-ri-gallery-view',
+      color: 'var(--va-c-text)',
+    }
+  ]
+
 })
